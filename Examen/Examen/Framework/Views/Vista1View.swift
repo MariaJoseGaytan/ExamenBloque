49e044d3f5ba92d17feec80da30231df90aef856
MariaJoseGaytan <A01706616@tec.mx>
2024-10-06 17:15:06 -0600

    vistas

---
+++ Examen/Examen/Framework/Views/Vista1View.swift
@@ -0,0 +1,22 @@
+//
+//  Vista1View.swift
+//  Examen
+//
+//  Created by Maria Jose Gaytan Gil on 06/10/24.
+//
+
+import SwiftUI
+
+struct Vista1: View {
+    var body: some View {
+        NavigationView {
+            Text("Contenido de Vista 1")
+                .navigationTitle("Vista 1")
+        }
+    }
+}
+
+#Preview {
+    Vista1()
+}
+
---
+++ Examen/Examen/Framework/Views/Vista2View.swift
@@ -0,0 +1,22 @@
+//
+//  Vista2View.swift
+//  Examen
+//
+//  Created by Maria Jose Gaytan Gil on 06/10/24.
+//
+
+import SwiftUI
+
+struct Vista2: View {
+    var body: some View {
+        NavigationView {
+            Text("Contenido de Vista 2")
+                .navigationTitle("Vista 2")
+        }
+    }
+}
+
+#Preview {
+    Vista2()
+}
+
